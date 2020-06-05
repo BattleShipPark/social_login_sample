7ce08427dee8d02916a53bb5ebfd44b5ffcee7a3
BattleShip Park <battleship.park@gmail.com>
2020-06-05 13:00:35 +0900

    페이스북 로그인,로그아웃

--- app/src/main/java/com/battleshippark/socialloginsample/FacebookActivity.kt
+++ app/src/main/java/com/battleshippark/socialloginsample/FacebookActivity.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.battleshippark.socialloginsample.databinding.ActivityFacebookBinding
+import com.facebook.AccessToken
 import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
+import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 
 
@@ -22,24 +24,56 @@ class FacebookActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         callbackManager = CallbackManager.Factory.create()
+
         binding.loginButton.setPermissions("email")
+/*
         binding.loginButton.registerCallback(
             callbackManager,
             object : FacebookCallback<LoginResult> {
                 override fun onSuccess(result: LoginResult?) {
                     Log.i("SocialLogin", "success")
+                    updateUI()
                 }
 
                 override fun onCancel() {
                     Log.i("SocialLogin", "cancel")
+                    updateUI()
                 }
 
                 override fun onError(error: FacebookException?) {
                     Log.i("SocialLogin", "error: $error")
+                    updateUI()
                 }
             })
-    }
+*/
 
+        LoginManager.getInstance().registerCallback(callbackManager,
+            object : FacebookCallback<LoginResult?> {
+                override fun onSuccess(result: LoginResult?) {
+                    Log.i("SocialLogin", "success2")
+                    updateUI()
+                }
+
+                override fun onCancel() {
+                    Log.i("SocialLogin", "cancel2")
+                    updateUI()
+                }
+
+                override fun onError(error: FacebookException?) {
+                    Log.i("SocialLogin", "error2: $error")
+                    updateUI()
+                }
+            })
+
+        binding.customLoginButton.setOnClickListener {
+            LoginManager.getInstance()
+                .logInWithReadPermissions(this@FacebookActivity, listOf("email"))
+        }
+
+        binding.customLogoutButton.setOnClickListener {
+            LoginManager.getInstance().logOut()
+        }
+    }
 
     override fun onActivityResult(
         requestCode: Int,
@@ -48,5 +82,18 @@ class FacebookActivity : AppCompatActivity() {
     ) {
         callbackManager.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        updateUI()
+    }
+
+    private fun updateUI() {
+        if (AccessToken.isCurrentAccessTokenActive()) {
+            binding.status.text = "Logined"
+        } else {
+            binding.status.text = "Logouted"
+        }
     }
 }
